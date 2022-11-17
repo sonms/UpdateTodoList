3ea182a467cc5e1cfb00a255b48aa4a91f26bfc3
sonms <smsms5676@naver.com>
2022-11-17 21:46:58 +0900

    feature-DarkMode(SharedPref)

---
+++ app/src/main/java/com/example/mytodolist/SharedPref.kt
@@ -0,0 +1,23 @@
+package com.example.mytodolist
+
+import android.content.Context
+import android.content.SharedPreferences
+
+
+class SharedPref(context: Context) {
+    var mySharedPref: SharedPreferences
+
+    init {
+        mySharedPref = context.getSharedPreferences("filename", Context.MODE_PRIVATE)
+    }
+
+    fun setNightModeState(state: Boolean?) {
+        val editor = mySharedPref.edit()
+        editor.putBoolean("NightMode", state!!)
+        editor.commit()
+    }
+
+    fun loadNightModeState(): Boolean {
+        return mySharedPref.getBoolean("NightMode", false)
+    }
+}
