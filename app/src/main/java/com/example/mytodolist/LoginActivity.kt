594b043dfaf89f4b926b6ade71d973eb72bb5ad5
sonms <smsms5676@naver.com>
2022-11-28 19:47:09 +0900

    feature-Login, Account(..ing)

--- app/src/main/java/com/example/mytodolist/LoginActivity.kt
+++ app/src/main/java/com/example/mytodolist/LoginActivity.kt
@@ -3,7 +3,10 @@ package com.example.mytodolist
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.SyncStateContract
 import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,9 +15,15 @@ class LoginActivity : AppCompatActivity() {
 
         val loginBtn : Button = findViewById(R.id.test_btn)
         loginBtn.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
+            if (user_email.text.toString().isEmpty()) {
+                Toast.makeText(this,"Email을 입력해주세요", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("email", user_email.text.toString())
+                intent.putExtra("password", user_password.text.toString())
+                startActivity(intent)
+                finish()
+            }
         }
     }
 }
--- app/src/main/java/com/example/mytodolist/navigation/AccountFragment.kt
+++ app/src/main/java/com/example/mytodolist/navigation/AccountFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.mytodolist.R
+import com.example.mytodolist.databinding.FragmentAccountBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +22,7 @@ class AccountFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var accountBinding: FragmentAccountBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,8 +36,20 @@ class AccountFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false)
+        accountBinding = FragmentAccountBinding.inflate(inflater, container, false)
+
+        val email = activity?.intent!!.getStringExtra("email")
+        val password = activity?.intent!!.getStringExtra("password")
+
+
+        accountBinding.inputEmail.text = email
+        accountBinding.inputPassword.text = password
+
+        accountBinding.inputEmail.setOnClickListener {
+            println("click")
+        }
+
+        return accountBinding.root
     }
 
     companion object {
